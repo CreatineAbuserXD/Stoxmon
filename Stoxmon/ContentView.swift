95ba7079fedac1405182c461a3863410c8f853c2
Philip <philipstipi@gmx.at>
2026-08-30 14:56:29 +0200

    Initial Commit

---
+++ Stoxmon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stoxmon
+//
+//  Created by Philip Stipkovits on 30.08.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stoxmon/StoxmonApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoxmonApp.swift
+//  Stoxmon
+//
+//  Created by Philip Stipkovits on 30.08.26.
+//
+
+import SwiftUI
+
+@main
+struct StoxmonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
